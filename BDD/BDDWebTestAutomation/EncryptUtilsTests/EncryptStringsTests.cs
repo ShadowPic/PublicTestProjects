de21ecb1c695bb5d40f1b59a4378b283f45f8b65
Darren Rich <darrich@microsoft.com>
2018-07-31 12:35:26 -0700

    adding basic encryption

---
+++ BDD/BDDWebTestAutomation/EncryptUtilsTests/EncryptStringsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EncryptUtils;
+namespace EncryptUtilsTests
+{
+    [TestClass]
+    public class EncryptStringsTests
+    {
+        [TestMethod]
+        public void EncryptStringTest()
+        {
+            //arrange
+            string OriginalString = "This is not encrypted";
+            string EncryptionKey = "really lame encryption key";
+            //act
+            var EncryptedString = EncryptUtils.EncryptStrings.Encrypt(OriginalString, true, EncryptionKey);
+            var DecryptedString = EncryptUtils.EncryptStrings.Decrypt(EncryptedString, true, EncryptionKey);
+            //assert
+            Assert.AreEqual(OriginalString, DecryptedString);
+        }
+
+        [TestMethod]
+        public void DecryptStringTest()
+        { }
+    }
+}
---
+++ BDD/BDDWebTestAutomation/EncryptUtilsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EncryptUtilsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EncryptUtilsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a4db617-5bd2-4f01-93c5-f729b80de37b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
