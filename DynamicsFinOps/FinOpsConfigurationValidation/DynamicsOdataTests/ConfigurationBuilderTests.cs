56ad8fbdcaa6d1e9c9285152f18778b6e72f531b
Darren Rich <darrich@microsoft.com>
2018-12-10 10:39:58 -0800

    Adding configbuilder documentation and updated readme

---
+++ DynamicsFinOps/FinOpsConfigurationValidation/DynamicsOdataTests/ConfigurationBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DynamicsOdataTests
+{
+    [TestClass]
+    public class ConfigurationBuilderTests
+    {
+        [TestMethod]
+        public void VerifySetting()
+        {
+
+            //arrange
+            string expectedValue = "from configbuilder";
+            string actualValue = "not from configbuilder";
+            //act
+            actualValue = System.Configuration.ConfigurationSettings.AppSettings["TestConfig"];
+            //assert
+            Assert.AreEqual(actualValue, expectedValue);
+        }
+    }
+}
