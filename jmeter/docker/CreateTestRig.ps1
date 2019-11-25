cb1ccfe5349f2056be9d31f797590c041f0302f7
Darren Rich <darrich@microsoft.com>
2019-11-25 10:46:21 -0800

    adding  wait for tiller deploy

--- jmeter/docker/CreateTestRig.ps1
+++ jmeter/docker/CreateTestRig.ps1
@@ -32,4 +32,35 @@ log "Seting helm rbac"
 kubectl apply -f helm-rbac.yaml
 # may need to add code here to wait for the policy to be applied
 log "Starting tiller"
-helm init --service-account tiller
+helm init --service-account tiller  --wait
+log "Creating AKS Namespace"
+kubectl create namespace $tenant
+#timing issue may be here
+log "Installing nginx"
+helm install stable/nginx-ingress --namespace $tenant --set controller.replicaCount=1 --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux --wait
+log "Getting Public IP address"
+$nginxPublicIP = $(kubectl -n $tenant get service -o json|convertfrom-json).items.status.LoadBalancer.ingress.ip
+$AksVmResourceGroup=$(az aks list --query "[*].nodeResourceGroup" -o tsv)
+$PUBLICIPID=$(az network public-ip list --resource-group $AksVmResourceGroup --query "[?ipAddress=='$($nginxPublicIP)']|[?contains(ipAddress, '$IP')].[ id]" --output tsv)
+log "Public IP ID: $($PUBLICIPID)"
+if($PUBLICIPID == $null || $PUBLICIPID == "")
+{
+    log "Failed to get the public ip address id"
+    return;
+}
+return
+log "Adding DNS suffix"
+az network public-ip update --ids $PUBLICIPID --dns-name $SubDns
+kubectl create namespace cert-manager
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+# Install the CustomResourceDefinition resources separately 
+helm repo add jetstack https://charts.jetstack.io
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml 
+helm repo update
+log "Install the cert-manager Helm chart"
+helm install --name cert-manager --namespace cert-manager --version v0.8.0 jetstack/cert-manager --wait
+kubectl apply -n $namespace -f cluster-issuer-prod.yaml
+kubectl -n $tenant apply -f .\jmeter_grafana_deploy.yaml
+kubectl -n $tenant rollout status deployment jmeter-grafana
+kubectl -n $tenant apply -f .\jmeter_grafana_ingress-prod.yaml
+# the ingress has the fqdn of the grafana service.  this may require a helm chart to make this generic.
