38ce4743315d034af8919b7f696ffc081c7e4c75
Darren Rich <darrich@microsoft.com>
2019-12-17 12:52:40 -0800

    manipulating json

--- source/JmeterPipelineValidationTests/JmeterPipelineValidationTests/Config.cs
+++ source/JmeterPipelineValidationTests/JmeterPipelineValidationTests/Config.cs
@@ -10,9 +10,12 @@ namespace JmeterPipelineValidationTests
     {
         public static IConfiguration Configuration {
             get {
+                string configFileContents = File.ReadAllText("appsettings.json");
+                byte[] byteArray = Encoding.UTF8.GetBytes(configFileContents.Replace(@"\", @"\\"));
+                MemoryStream stream = new MemoryStream(byteArray);
                 var builder = new ConfigurationBuilder()
                .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json");
+               .AddJsonStream(stream);
                 return builder.Build();
             }}
 
