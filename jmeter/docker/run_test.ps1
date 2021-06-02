8c79ce6e6714e8a8e3719e576d9c3b4a5fff5925
Darren Rich <darrich@microsoft.com>
2021-06-01 23:49:05 -0700

    Adding chaos files and stuff

--- jmeter/docker/run_test.ps1
+++ jmeter/docker/run_test.ps1
@@ -126,7 +126,7 @@ Write-Output "Starting test execution on AKS Cluster"
 kubectl -n $tenant exec $MasterPod -- /load_test_run "/$(Split-Path $TestName -Leaf)" $GlobalJmeterParams
 Write-Output "Retrieving dashboard, results and Master jmeter.log"
 kubectl cp $tenant/${MasterPod}:/report $ReportFolder
-kubectl cp $tenant/${MasterPod}:/results.log $ReportFolder/results.log
+kubectl cp $tenant/${MasterPod}:/results.log $ReportFolder/results.jtl
 kubectl cp $tenant/${MasterPod}:/jmeter/apache-jmeter-5.3/bin/jmeter.log $ReportFolder/jmeter.log
 if($DeleteTestRig)
 {
