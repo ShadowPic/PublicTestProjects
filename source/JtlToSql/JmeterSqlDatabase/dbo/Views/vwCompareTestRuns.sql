c87dc096c6e069898ca0a146e2d826dabf3d18aa
Darren Rich <darrich@microsoft.com>
2025-01-12 17:02:45 -0800

    getting ahead of myself

--- source/JtlToSql/JmeterSqlDatabase/dbo/Views/vwCompareTestRuns.sql
+++ source/JtlToSql/JmeterSqlDatabase/dbo/Views/vwCompareTestRuns.sql
@@ -4,6 +4,6 @@ select TestPlan,TestRun,label,avg(elapsed) "AverageResponseTimeMS"
 ,max(elapsed) "MaxResponseTimeMS",count(label) "Samples",CONVERT(float,(sum(TransactionFailedSamples))/CONVERT(float,count(label)))"ErrorPercent",max(allThreads) "Users"
 , sum(TransactionSamples)"TransactionSamplers",sum(TransactionFailedSamples)"TransactionFailedSamplers",avg(bytes) "AvgBytes"
     from jmeterrawresults
-        where IsTransaction=1
+        where responseMessage like 'Number of samples in transaction%'
             and threadName not in (select ThreadName from ExcludedThreadNames)
         group by TestPlan,TestRun,label
