3ee5f026cb188027e349f4e85a79276f1f3412e2
Darren Rich <darrich@microsoft.com>
2025-01-12 18:05:40 -0800

    tweaking

--- source/JtlToSql/JmeterSqlDatabase/dbo/Views/vwCompareTestRuns.sql
+++ source/JtlToSql/JmeterSqlDatabase/dbo/Views/vwCompareTestRuns.sql
@@ -1,9 +1,9 @@
 ﻿CREATE view [dbo].[vwCompareTestRuns]
 as
 select TestPlan,TestRun,label,avg(elapsed) "AverageResponseTimeMS"
-,max(elapsed) "MaxResponseTimeMS",count(label) "Samples",CONVERT(float,(sum(TransactionFailedSamples))/CONVERT(float,count(label)))"ErrorPercent",max(allThreads) "Users"
+,max(elapsed) "MaxResponseTimeMS",count(label) "Samples",case when sum(TransactionFailedSamples)>0 then CONVERT(float,(sum(TransactionFailedSamples))/CONVERT(float,count(label)))else 0 end "ErrorPercent",max(allThreads) "Users"
 , sum(TransactionSamples)"TransactionSamplers",sum(TransactionFailedSamples)"TransactionFailedSamplers",avg(bytes) "AvgBytes"
     from jmeterrawresults
-        where responseMessage like 'Number of samples in transaction%'
+        where IsTransaction=1
             and threadName not in (select ThreadName from ExcludedThreadNames)
         group by TestPlan,TestRun,label
